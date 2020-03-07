322c21f2e7c0d7089bf48b1ac766e4f0f605e2c4
root <igorareche@gmail.com>
2020-03-07 08:04:46 -0300

    first commit

--- javascript.js
+++ javascript.js
@@ -1,7 +1,3 @@
-
-
-//if (nome == 'admin' && senha =='123'){
-	//window.location.href = 'first-page.html'
 function entrarnosite(){
 	var nome = document.getElementById('nome').value
 	var senha = document.getElementById('senha').value
@@ -9,7 +5,7 @@ function entrarnosite(){
 
 	if (nome == "admin" && senha == 123){
 	
-	window.location.assign ('first-page.html')
+	window.open('first-page.html')
 	}
 	else{
 		alert("Por favor digite um usuário válido")
