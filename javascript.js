92a8ce24eda8760738053c0b770bdb78668779c0
IgorAReche <igorareche@gmail.com>
2020-05-10 14:46:14 -0300

    Uploader

--- javascript.js
+++ javascript.js
@@ -27,12 +27,80 @@ window.onclick = function(event) {
   }
 } 
 }
-function uploader() {
-	var x = document.createElement("INPUT");
-	x.setAttribute("type", "file");
-	document.body.appendChild(x);
-  }
-  
+function Upload() {
+        var fileUpload = document.getElementById("fileUpload");
+        var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.xls|.xlsx)$/;
+        if (regex.test(fileUpload.value.toLowerCase())) {
+            if (typeof (FileReader) != "undefined") {
+                var reader = new FileReader();
+                if (reader.readAsBinaryString) {
+                    reader.onload = function (e) {
+                        ProcessExcel(e.target.result);
+                    };
+                    reader.readAsBinaryString(fileUpload.files[0]);
+                } else {
+                    reader.onload = function (e) {
+                        var data = "";
+                        var bytes = new Uint8Array(e.target.result);
+                        for (var i = 0; i < bytes.byteLength; i++) {
+                            data += String.fromCharCode(bytes[i]);
+                        }
+                        ProcessExcel(data);
+                    };
+                    reader.readAsArrayBuffer(fileUpload.files[0]);
+                }
+            } else {
+                alert("This browser does not support HTML5.");
+            }
+        } else {
+            alert("Please upload a valid Excel file.");
+        }
+    };
+    function ProcessExcel(data) {
+        var workbook = XLSX.read(data, {
+            type: 'binary'
+        });
+ 
+        var firstSheet = workbook.SheetNames[0];
+ 
+        var excelRows = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[firstSheet]);
+ 
+        var table = document.createElement("table");
+        table.border = "1";
+ 
+		var row = table.insertRow(-1);
+		
+        var headerCell = document.createElement("TH");
+        headerCell.innerHTML = "Id";
+        row.appendChild(headerCell);
+ 
+        headerCell = document.createElement("TH");
+        headerCell.innerHTML = "Name";
+        row.appendChild(headerCell);
+ 
+        headerCell = document.createElement("TH");
+        headerCell.innerHTML = "Country";
+		row.appendChild(headerCell);
+		
+        for (var i = 0; i < excelRows.length; i++) {
+            var row = table.insertRow(-1);
+ 
+            
+            var cell = row.insertCell(-1);
+            cell.innerHTML = excelRows[i].Id;
+ 
+            cell = row.insertCell(-1);
+            cell.innerHTML = excelRows[i].Name;
+ 
+            cell = row.insertCell(-1);
+            cell.innerHTML = excelRows[i].Country;
+        }
+ 
+        var dvExcel = document.getElementById("dvExcel");
+        dvExcel.innerHTML = "";
+        dvExcel.appendChild(table);
+    };
+
 function entradadosdados(){
 	var splitados = new Array();
 	var i;
